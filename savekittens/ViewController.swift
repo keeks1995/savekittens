a9ec541f5ad0a4a4c19feac44dde7ac7359bdd62
Alex Kikos <alexkikos@coe-mac-1557.local>
2018-01-13 12:28:35 -0600

    Initial Commit

---
+++ savekittens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  savekittens
+//
+//  Created by Alex Kikos on 1/13/18.
+//  Copyright © 2018 Alex Kikos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
